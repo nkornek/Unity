cd4f6054220cffe30c17b7b9e5e1515fdd67d5c4
nkornek <nkornek@gmail.com>
2014-02-11 00:57:41 -0500

    hello

--- Test/Assets/Scripts/LessonScript.cs
+++ Test/Assets/Scripts/LessonScript.cs
@@ -1,18 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Operators : MonoBehaviour 
-	{
+public class LessonScript : MonoBehaviour 
+{
 	
 	// Use this for initialization
 	void Start () 
-		{
-		
-		}
+	{
+		int num = 5;
+
+		print ("This number is " + (num % 2 == 0 ? "even" : "odd"));
+	}
 	
 	// Update is called once per frame
 	void Update () 
-		{
+	{
 		
-		}
 	}
+}
