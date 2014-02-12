35bf8e303040de9f29b1898eebe01f811afe351b
nkornek <nkornek@gmail.com>
2014-02-12 17:14:09 -0500

    Began work on Primitive

---
+++ Primitive/Assets/RedPlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RedPlayerMovement : MonoBehaviour 
+{
+	bool canMove;
+
+	// Use this for initialization
+	void Start () 
+	{
+		canMove = true;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if canMove == true
+		{
+
+		}
+	}
+}
