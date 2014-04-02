185658071093cf0e40257e59fdff95b7d3d90269
nkornek <nkornek@gmail.com>
2014-04-02 15:23:25 -0400

    glasses

---
+++ Glasses/Assets/Toggleglasses.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Toggleglasses : MonoBehaviour 
+{
+	public bool glassesVisible;
+
+	// Use this for initialization
+	void Start () 
+	{
+		glassesVisible = true;
+		}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetKeyUp("1") & glassesVisible == false)
+		{
+			glassesVisible = true;
+		}
+		if(Input.GetKeyUp("1") & glassesVisible == true)
+		{
+			glassesVisible = false;
+		}
+		if (glassesVisible == true)
+		{
+			gameObject.GetComponent<MeshRenderer>().enabled=true;
+		}
+		else
+		{
+			gameObject.GetComponent<MeshRenderer>().enabled=false;
+		}
+	}
+}
